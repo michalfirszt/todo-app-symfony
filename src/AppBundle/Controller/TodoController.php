2e47855d451c0765f53ae1c56e6d55a1faa777bb
michalfirszt <michalfiszu@gmail.com>
2018-08-20 20:36:53 +0200

    display-data-on-the-homepage

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Todo;
 
 class TodoController extends Controller
 {
@@ -13,7 +14,12 @@ class TodoController extends Controller
      */
     public function indexAction()
     {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $tasks = $entityManager
+                ->getRepository(Todo::class)
+                ->findOverview();
         
-        return $this->render('todo/index.html.twig');
+        return $this->render('todo/index.html.twig', ['tasks' => $tasks]);
     }
 }
