f9d36f19adf2668ce3d28363c94b3c0548d7fb30
michalfirszt <michalfiszu@gmail.com>
2018-09-02 14:31:49 +0200

    add-create-action

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -5,10 +5,24 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use AppBundle\Entity\Todo;
 
 class TodoController extends Controller
 {
+    private $categories = ['Homework' => 'homework', 
+                            'Job' => 'job', 
+                            'Rest' => 'rest',
+                            'Sport' => 'sport'];
+
+    private $priorities = ['Low' => 'low', 
+                            'Medium' => 'medium', 
+                            'High' => 'high'];
+
     /**
      * @Route("/", name="homepage")
      */
@@ -42,5 +56,51 @@ class TodoController extends Controller
                 ->find($id);
         
         return $this->render('todo/details.html.twig', ['task' => $task]);
+    }
+
+    /**
+     * @Route("/create", name="create_page")
+     * 
+     * @return Response
+     */
+    public function createAction(Request $request)
+    {
+        $task = new Todo();
+
+        $createForm = $this->createFormBuilder($task)
+                        ->add('name', TextType::class)
+                        ->add('description', TextareaType::class)
+                        ->add('category', ChoiceType::class, ['choices' => $this->categories])
+                        ->add('priority', ChoiceType::class, ['choices' => $this->priorities])
+                        ->add('dueDate', DateType::class)
+                        ->add('submit', SubmitType::class)
+                        ->getForm();
+
+        $createForm->handleRequest($request);
+
+        if($createForm->isSubmitted() && $createForm->isValid())
+        {
+            $currentDate = new\DateTime('now');
+
+            $task->setName($createForm['name']->getData());
+            $task->setDescription($createForm['description']->getData());
+            $task->setCategory($createForm['category']->getData());
+            $task->setPriority($createForm['priority']->getData());
+            $task->setDueDate($createForm['dueDate']->getData());
+            $task->setCreateDate($currentDate);
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Task Added'
+            );
+
+            return $this->redirectToRoute('homepage');
+        }
+        
+        return $this->render('todo/create.html.twig', ['form' => $createForm->createView()]);
     }
 }
